d3bfa83f8c98050be76c3a7f0dcd2f29e4373a8b
Daniel Fabian <daniel.fabian@integral-it.ch>
2015-03-31 21:52:33 +0200

    go stack.

--- Go/stack.go
+++ Go/stack.go
@@ -1,9 +1,28 @@
-package Go
+package main
 
 type Stack struct  {
     data []interface{}
 }
 
-func (s *Stack) size() {
+func (s *Stack) size() int {
     return len(s.data)
+}
+
+func (s *Stack) push(item interface{}) {
+    s.data = append(s.data, item)
+}
+
+func (s *Stack) peek() interface{} {
+    return s.data[len(s.data) - 1]
+}
+
+func (s *Stack) pop() interface{} {
+    idx := len(s.data) - 1
+    res := s.data[idx]
+    s.data = s.data[0:idx]
+    return res
+}
+
+func (s *Stack) isEmpty() bool {
+    return len(s.data) == 0
 }
--- Go/stack_test.go
+++ Go/stack_test.go
@@ -1,9 +1,50 @@
-package Go
+package main
 import "testing"
 
 func Test_size(t *testing.T) {
     testee := new(Stack)
     if testee.size() != 0 {
-        t.Errorf("size of empty stack should be 0");
+        t.Errorf("size of empty stack should be 0")
+    }
+    testee.push(1)
+    if testee.size() != 1 {
+        t.Errorf("size after one push, should be 1")
+    }
+}
+
+func Test_push(t *testing.T) {
+    testee := new(Stack)
+    testee.push(1)
+
+    if testee.peek() != 1 {
+        t.Errorf("the top item should be read properly")
+    }
+}
+
+func Test_pop(t *testing.T) {
+    testee := new(Stack)
+    testee.push(1)
+    testee.push(2)
+    res := testee.pop()
+
+    if res != 2 {
+        t.Errorf("the popped item should be the same as the one put in")
+    }
+
+    if testee.size() != 1 {
+        t.Errorf("after removing, the stack should have length 1")
+    }
+}
+
+func Test_isEmpty(t *testing.T) {
+    testee := new(Stack)
+    if testee.isEmpty() != true {
+        t.Errorf("An initial stack is empty")
+    }
+
+    testee.push(1)
+
+    if testee.isEmpty() {
+        t.Errorf("After pushing the stack is no longer empty")
     }
 }

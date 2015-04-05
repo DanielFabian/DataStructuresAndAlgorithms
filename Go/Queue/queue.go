a317aa0e99fb48e161255d3988dc92fb2f354aaa
Daniel Fabian <daniel.fabian@integral-it.ch>
2015-04-05 13:40:12 +0200

    Add bad go queue: linerarly growing memory.

--- Go/Queue/queue.go
+++ Go/Queue/queue.go
@@ -1,1 +1,24 @@
 package Queue
+
+// bad implementation of a queue with linearly growing memory consumption.
+type Queue struct {
+    data []interface{}
+}
+
+func (q *Queue) size() int {
+    return len(q.data)
+}
+
+func (q *Queue) enqueue(item interface{}) {
+    q.data = append(q.data, item)
+}
+
+func (q *Queue) dequeue() interface{} {
+    res := q.data[0];
+    q.data = q.data[1:]
+    return res
+}
+
+func (q *Queue) isEmpty() bool {
+    return len(q.data) == 0
+}

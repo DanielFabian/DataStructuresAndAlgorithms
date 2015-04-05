6d94d5e4816d1c1e3297fdc9eab4d431ef549251
Daniel Fabian <daniel.fabian@integral-it.ch>
2015-04-05 13:41:26 +0200

    Add bad F# queue: O(n) dequeue.

---
+++ F#/F#/queue.fs
@@ -0,0 +1,13 @@
+﻿module FSharp.Queue
+
+
+/// bad implementation of a queue with O(n) dequeue
+type Queue<'T>() =
+    let data = ResizeArray<'T>()
+    member this.size = data.Count
+    member this.enqueue item = data.Add item
+    member this.dequeue() =
+        let res = data.[0]
+        data.RemoveAt 0
+        res
+    member this.isEmpty = data.Count = 0
---
+++ F#/Test/queue.fs
@@ -0,0 +1,31 @@
+﻿module FSharp.Queue
+
+open FSharp.Queue
+open FsUnit
+open NUnit.Framework
+
+let [<Test>] ``the size should be correct`` () =
+    let testee = Queue()
+    testee.size |> should equal 0
+    testee.enqueue 1
+    testee.size |> should equal 1
+
+let [<Test>] ``enqueue should be correct`` () =
+    let testee = Queue()
+    testee.enqueue 1
+    testee.dequeue() |> should equal 1
+
+let [<Test>] ``dequeue should be correct`` () =
+    let testee = Queue()
+    testee.enqueue 1
+    testee.enqueue 2
+    testee.dequeue() |> should equal 1
+    testee.size |> should equal 1
+
+let [<Test>] ``isEmpty should be correct`` () =
+    let testee = Queue()
+    testee.isEmpty |> should equal true
+    testee.enqueue 1
+    testee.isEmpty |> should equal false
+
+

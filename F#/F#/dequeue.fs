827bdd503dd8563d63473e0703ce6a050d2e16f7
Daniel Fabian <daniel.fabian@incubeadvisory.com>
2015-04-06 09:59:43 +0200

    F# dequeue.

---
+++ F#/F#/dequeue.fs
@@ -0,0 +1,23 @@
+﻿module FSharp.Dequeue
+
+type Dequeue<'T>() =
+    let data = ResizeArray<'T>()
+    
+    member this.addFront item = 
+        data.Insert(0, item)
+    
+    member this.addRear item =
+        data.Add item
+    
+    member this.removeRear () = 
+        let res = data.[data.Count - 1]
+        data.RemoveAt(data.Count - 1)
+        res
+
+    member this.removeFront () =
+        let res = data.[0]
+        data.RemoveAt(0)
+        res
+    
+    member this.size () = data.Count
+
